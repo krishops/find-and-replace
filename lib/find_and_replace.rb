c2d65a4a0d627d43429fa6534994bb77790a7908
Kris Hopper <hopperdavis@gmail.com>
2021-07-14 13:49:41 -0700

    first commit

---
+++ lib/find_and_replace.rb
@@ -0,0 +1,5 @@
+class String
+  def find_and_replace(s1, s2)
+
+  end
+end
---
+++ spec/find_and_replace_spec.rb
@@ -0,0 +1,7 @@
+require('rspec')
+require('find_and_replace')
+require('pry')
+
+
+
+"Hello world".find_and_replace("world", "universe")
